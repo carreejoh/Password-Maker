e0137ac925f2715a93d5f932054d488aa8b72172
Carter <carreejoh4637@gmail.com>
2023-03-12 16:48:45 -0600

    Finally got random selector/builder to work

--- assets/script.js
+++ assets/script.js
@@ -21,13 +21,20 @@ var pswdchardown = false;
 var pswdcharnum = false;
 var pswdcharspec = false;
 
+var uppercasechar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercasechar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var intergerchar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialchar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "<", ">", "/", "=", "+"];
+
+var finalpassword = [];
+
 function pswdprompts() {
   var pswdlength = prompt("Enter a length for password, must be between 8-128 characters"); 
   var pswdlengthnumber = Number(pswdlength)
   if (pswdlengthnumber <= 7 || pswdlengthnumber >= 128) {
+    alert("Invalid Length");
     return false;
  } else {
-   console.log(pswdlengthnumber);
    return pswdlengthnumber;
  }
 }
@@ -39,11 +46,38 @@ function pswdchar() {
   pswdcharspec = confirm("Do you want special characters in your password? Ok for yes, Cancel for no");
   if (pswdcharup == false && pswdchardown == false && pswdcharnum == false && pswdcharspec == false) {
     alert("No character types were picked");
-  } 
- 
+  } else {
+    
+  }
 }
 
-
+function passwordMaker() {
+  pswdlength = pswdprompts()
+  if (pswdlength !== false) {
+    pswdchar();
+    for (var i = 0; i < pswdlength; i++) {
+      var randomtype = Math.floor(Math.random() * 4);
+      if (pswdcharup === true && randomtype === 0) {
+        var randomchar = Math.floor(Math.random() * uppercasechar.length);
+        finalpassword.push(uppercasechar[randomchar]);
+       } else if (pswdchardown === true && randomtype === 1) {
+        var randomchar1 = Math.floor(Math.random() * lowercasechar.length);
+        finalpassword.push(lowercasechar[randomchar1]);
+       } else if (pswdcharnum === true && randomtype === 2) {
+        var randomchar2 = Math.floor(Math.random() * intergerchar.length);
+        finalpassword.push(intergerchar[randomchar2]);
+       } else if (pswdcharspec === true && randomtype === 3) {
+        var randomchar3 = Math.floor(Math.random() * specialchar.length);
+        finalpassword.push(specialchar[randomchar3]);
+       } else {
+        i--
+       }
+       var passwordText = document.querySelector("#password");
+       passwordText.value = finalpassword;
+  }
+  console.log(finalpassword);
+ }
+}
 
 
 function writePassword() {
@@ -64,4 +98,4 @@ function writePassword() {
 
 
 
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordMaker);
